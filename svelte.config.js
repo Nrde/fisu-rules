ba47df3d3bcf0f13ea37cb280d92bd61ce0a0d13
Panu Artimo <tuntematon@gmail.com>
2026-04-04 14:30:08 +0200

    Update Svelte configuration to use vercel runtime adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
+/*import adapter from '@sveltejs/adapter-static';*/
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+/*const config = {
   kit: {
     adapter: adapter({
       pages: 'build',
@@ -9,6 +9,14 @@ const config = {
       fallback: 'index.html'
     })
   }
+};*/
+
+const config = {
+  kit: {
+    adapter: adapter({
+      runtime: 'nodejs24.x',
+    }),
+  },
 };
 
 export default config;
